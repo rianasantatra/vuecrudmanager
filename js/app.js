56be083f672252540e40918b8a253f08acb405c1
Riana Santatra RAZAFIMAMONJY - Charbon <rianasantatra665@gmail.com>
2023-06-14 14:33:34 +0300

    creation route home, form, list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
-const App = {};
+const App = {
+  data() {
+    return {
+      showHome: false,
+      showCreateForm: false,
+      showStudentsList: false,
+    };
+  },
+
+  methods: {
+    goToHome() {
+      this.showHome = true;
+    },
+    goToCreateForm() {
+      this.showCreateForm = true;
+    },
+    goToStudentsList() {
+      this.showStudentsList = true;
+    },
+  },
+};
 
 Vue.createApp(App).mount("#app");
